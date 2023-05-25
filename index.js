e78640e29f7879d0a91c9b019fd9cfbf4fa4619c
Eldio Neto <eldioneto2021@hmail.com>
2023-05-24 21:53:10 -0400

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
-const apiRoutes = require('./routes/api');
+const path = require ('path');
 
-const PORT = 3000;
+const PORT = 4000;
 const app = express();
-app.use('/a',apiRoutes);
+
+const users = [
+    {id:0, name: 'eldio', phone: '123456789'},
+    {id:1, name: 'eldio', phone: '123456789'},
+    {id:2, name: 'eldio', phone: '123456789'},
+    
+]
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/',(req, res)=>{
+    res.render('user', {users})
+});
 
 app.listen(PORT, ()=>{
     console.log("server running on port " + PORT);
